239a8486125e941b755de4961062ffcaf1b95207
Charles Harvey <harvey.charles@gmail.com>
2019-02-05 11:24:58 +0100

    reverse column order in frontpage

--- front-page.php
+++ front-page.php
@@ -30,21 +30,8 @@
         <section>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-8">
-                        <h2>Vous êtes :</h2>
-<?php $people = array(
-'propritetaire' => 'Un particulier propritétaire',
-'entreprise' => 'Une entreprise',
-'gerance' => 'Une régie ou gérance ',
-'installateur' => 'Un installateur électrique'
-); ?>
-                        <ul id="services_for">
-                            <?php foreach ($people as $person => $description) : ?>
-                                <li data-person="<?php echo $person; ?>"  id="s_<?php echo $person; ?>"><?php echo $description; ?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
-                    <div class="col-sm-4">
+
+                    <div class="col-sm-4 col-sm-push-8">
                         <div id="formation_securelec">
                             <h2>Formations Securelec</h2>
                             <div class="formation_picture">PICTURE HERE</div>
@@ -65,7 +52,24 @@
                             <a href="#" class="button">Toutes nos formations</a>
                         </div>
 
-                    </div>
+                    </div> <!-- END OF col-sm-4 -->
+
+
+                    <div class="col-sm-8 col-sm-pull-4">
+                        <h2>Vous êtes :</h2>
+                        <?php $people = array(
+                            'propritetaire' => 'Un particulier propritétaire',
+                            'entreprise' => 'Une entreprise',
+                            'gerance' => 'Une régie ou gérance ',
+                            'installateur' => 'Un installateur électrique'
+                        ); ?>
+                        <ul id="services_for">
+                            <?php foreach ($people as $person => $description) : ?>
+                                <li data-person="<?php echo $person; ?>"  id="s_<?php echo $person; ?>"><?php echo $description; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div><!-- END OF col-sm-8 -->
+
                 </div>
 
             </div>
@@ -76,48 +80,48 @@
 
 
                 <div id="people_boxes">
-                <div id="arrow" >&nbsp;</div>
+                    <div id="arrow" >&nbsp;</div>
 
 
-                <?php foreach ($people as $person => $description) : ?>
+                    <?php foreach ($people as $person => $description) : ?>
 
-                <div class="person_box" id="<?php echo $person; ?>">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <?php echo $person; ?>
-                            <?php echo $person; ?>
-                            <?php echo $person; ?>
-                            <?php echo $person; ?>
-                            <img src="" alt="" />
-                        </div>
-                        <div class="col-sm-8">
-                            <h3>Contrôle d’inspection périodique ou vente d’un bien immobilier</h3>
-                            <p>Votre installation électrique doit subir périodiquement un contrôle. La périodicité de ce contrôle dépend des locaux où se situe l’installation électrique. Outre le contrôle périodique échu, pour les installations dont la périodicité est 10 ans ou 20 ans, lors de la vente d’un bien immobilier, un contrôle périodique est nécessaire si le dernier contrôle date de plus de cinq ans. Prenez rendez-vous avec un de nos inspecteurs certifiés qui réaliseront votre contrôle d’inspection sur les cantons de Genève et Vaud.</p>
-                            <a href="#" class="button">En savoir plus</a>
-                        </div>
-                    </div>
-                </div><!--  END OF grey_box -->
-            <?php endforeach; ?>
+                        <div class="person_box" id="<?php echo $person; ?>">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <?php echo $person; ?>
+                                    <?php echo $person; ?>
+                                    <?php echo $person; ?>
+                                    <?php echo $person; ?>
+                                    <img src="" alt="" />
+                                </div>
+                                <div class="col-sm-8">
+                                    <h3>Contrôle d’inspection périodique ou vente d’un bien immobilier</h3>
+                                    <p>Votre installation électrique doit subir périodiquement un contrôle. La périodicité de ce contrôle dépend des locaux où se situe l’installation électrique. Outre le contrôle périodique échu, pour les installations dont la périodicité est 10 ans ou 20 ans, lors de la vente d’un bien immobilier, un contrôle périodique est nécessaire si le dernier contrôle date de plus de cinq ans. Prenez rendez-vous avec un de nos inspecteurs certifiés qui réaliseront votre contrôle d’inspection sur les cantons de Genève et Vaud.</p>
+                                    <a href="#" class="button">En savoir plus</a>
+                                </div>
+                            </div>
+                        </div><!--  END OF grey_box -->
+                    <?php endforeach; ?>
 
-</div>
                 </div>
-            </section>
-
-
-
-            <?php include('section-loop.php'); ?>
-
-
-            <div class="container">
-                <?php the_content(); ?>
             </div>
+        </section>
 
 
 
-        </article>
-        <!-- /article -->
+        <?php include('section-loop.php'); ?>
 
-    <?php endwhile; ?>
+
+        <div class="container">
+            <?php the_content(); ?>
+        </div>
+
+
+
+    </article>
+    <!-- /article -->
+
+<?php endwhile; ?>
 
 <?php else: ?>
 
