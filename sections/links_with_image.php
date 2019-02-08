cd05ed78380d63b54bb83970beef5db51f415084
Charles Harvey <harvey.charles@gmail.com>
2019-02-08 12:06:53 +0100

    styled link with image

--- sections/links_with_image.php
+++ sections/links_with_image.php
@@ -10,15 +10,17 @@
                 <?php $page = get_sub_field('page'); ?>
                 <?php $url =  get_permalink($page->ID); ?>
                 <div class="page_link">
-                    <?php if ($image): ?>
-                        <a href="<?php echo $url; ?>">
-                            <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $page->post_title; ?>">
-                        </a>
-                    <?php endif; ?>
-                    <h3><a href="<?php echo $url; ?>"><?php echo $page->post_title; ?></a></h3>
+                    <a href="<?php echo $url; ?>">
+                        <?php if ($image): ?>
+
+                            <span class="page_link_image" style="background-image:url(<?php echo $image['sizes']['medium']; ?>)" ></span>
+
+                        <?php endif; ?>
+                        <strong><?php echo $page->post_title; ?></strong>
+                    </a>
                 </div>
             <?php endwhile; ?>
         </div>
     </div>
-    
+
 <?php endif; ?>
